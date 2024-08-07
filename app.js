41c1cf8d5fe4a0b45c369532fd64f5de712ec0c7
siva <msivak0814@gmail.com>
2024-08-07 15:20:42 -0700

    changed the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing Two More'));
+app.get('/', (req, res) => res.send('Happy Learning, AWS!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
